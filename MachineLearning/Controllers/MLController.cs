9285fb41907bb4cd69b4a105e2f68dc3211227f9
dangthanh03 <dangtrithanh03@gmail.com>
2024-06-11 19:45:31 +0700

    first commit

---
+++ MachineLearning/Controllers/MLController.cs
@@ -0,0 +1,76 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using MachineLearning.Models;
+
+public class MLController : Controller
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+
+    public MLController(IHttpClientFactory httpClientFactory)
+    {
+        _httpClientFactory = httpClientFactory;
+    }
+
+    [HttpGet]
+    public IActionResult Input()
+    {
+        return View(new PredictionInputModel());
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Predict(PredictionInputModel input)
+    {
+        var client = _httpClientFactory.CreateClient();
+        var apiUrl = "https://c02e-104-199-152-77.ngrok-free.app/predict"; // URL từ ngrok
+
+        var inputData = new
+        {
+            gender = input.Gender,
+            customer_type = input.CustomerType,
+            age = input.Age,
+            type_of_travel = input.TypeOfTravel,
+            Travelclass = input.Class,
+            flight_distance = input.FlightDistance,
+            inflight_wifi_service = input.InflightWifiService,
+            departure_arrival_time_convenient = input.DepartureArrivalTimeConvenient,
+            ease_of_online_booking = input.EaseOfOnlineBooking,
+            gate_location = input.GateLocation,
+            food_and_drink = input.FoodAndDrink,
+            online_boarding = input.OnlineBoarding,
+            seat_comfort = input.SeatComfort,
+            inflight_entertainment = input.InflightEntertainment,
+            on_board_service = input.OnBoardService,
+            leg_room_service = input.LegRoomService,
+            baggage_handling = input.BaggageHandling,
+            checkin_service = input.CheckinService,
+            inflight_service = input.InflightService,
+            cleanliness = input.Cleanliness,
+            departure_delay_in_minutes = input.DepartureDelayInMinutes,
+            arrival_delay_in_minutes = input.ArrivalDelayInMinutes
+        };
+
+        var content = new StringContent(JsonConvert.SerializeObject(inputData), Encoding.UTF8, "application/json");
+
+        var response = await client.PostAsync(apiUrl, content);
+        if (response.IsSuccessStatusCode)
+        {
+            var responseString = await response.Content.ReadAsStringAsync();
+            var predictionResponse = JsonConvert.DeserializeObject<Dictionary<string, int>>(responseString);
+            if (predictionResponse != null && predictionResponse.ContainsKey("prediction"))
+            {
+                var predictionValue = predictionResponse["prediction"];
+                ViewBag.Prediction = predictionValue == 0 ? "The customer is satisfiied" : "The customer is neutral or dissatisfied";
+                
+            }
+        }
+        else
+        {
+            ViewBag.Prediction = "Error in prediction API call.";
+        }
+
+        return View("Input", input);
+    }
+}
---
+++ MachineLearning/Models/PredictionInputModel.cs
@@ -0,0 +1,28 @@
+﻿namespace MachineLearning.Models
+{
+    public class PredictionInputModel
+    {
+        public string Gender { get; set; }
+        public string CustomerType { get; set; }
+        public int Age { get; set; }
+        public string TypeOfTravel { get; set; }
+        public string Class { get; set; }
+        public int FlightDistance { get; set; }
+        public int InflightWifiService { get; set; }
+        public int DepartureArrivalTimeConvenient { get; set; }
+        public int EaseOfOnlineBooking { get; set; }
+        public int GateLocation { get; set; }
+        public int FoodAndDrink { get; set; }
+        public int OnlineBoarding { get; set; }
+        public int SeatComfort { get; set; }
+        public int InflightEntertainment { get; set; }
+        public int OnBoardService { get; set; }
+        public int LegRoomService { get; set; }
+        public int BaggageHandling { get; set; }
+        public int CheckinService { get; set; }
+        public int InflightService { get; set; }
+        public int Cleanliness { get; set; }
+        public int DepartureDelayInMinutes { get; set; }
+        public float ArrivalDelayInMinutes { get; set; }
+    }
+}
